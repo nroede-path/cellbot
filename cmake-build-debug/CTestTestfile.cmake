c926c6765a18890b78cfcb38987608cab9fdfa92
nroede-path <105659003+nroede-path@users.noreply.github.com>
2022-07-20 14:40:37 -0400

    delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nroede/rosproj/ur_ws/src/cellbot
-# Build directory: /home/nroede/rosproj/ur_ws/src/cellbot/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
