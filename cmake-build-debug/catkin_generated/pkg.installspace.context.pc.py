30e4b73de15979d1780f60c98f746608d1b5c280
nroede-path <nroede@path-robotics.com>
2022-07-21 16:05:09 -0400

    Revert "delete cmake-build-debug directory"
    This reverts commit c926c676

---
+++ cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "moveit_ros_planning;moveit_ros_planning_interface;moveit_visual_tools;rviz_visual_tools;cv_bridge;sensor_msgs;roscpp;std_msgs;image_transport".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "cellbot"
+PROJECT_SPACE_DIR = "/usr/local"
+PROJECT_VERSION = "0.0.0"
---
+++ cmake-build-debug/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/nroede/rosproj/ur_ws/src/cellbot/cmake-build-debug/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
